730b30aa3a9beec9689cf68992abb4eca6741545
Ola Pawelec <olapawelec@MacBook-Pro-MacOS.local>
2020-03-13 17:30:32 +0100

    Updates

---
+++ precache-manifest.1e5e12fcc86a91f51e91bb2fe9e4e048.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3053231a0be6a2e7cb5831b6a710ceb1",
+    "url": "/SPA-with-React-Router/index.html"
+  },
+  {
+    "revision": "3646cc3b010008b78c96",
+    "url": "/SPA-with-React-Router/static/css/main.7bdc269b.chunk.css"
+  },
+  {
+    "revision": "3cca412c8907a136624a",
+    "url": "/SPA-with-React-Router/static/js/2.57eaccdd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SPA-with-React-Router/static/js/2.57eaccdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3646cc3b010008b78c96",
+    "url": "/SPA-with-React-Router/static/js/main.89ad65c7.chunk.js"
+  },
+  {
+    "revision": "28ced803b7d928e45295",
+    "url": "/SPA-with-React-Router/static/js/runtime-main.39f87de6.js"
+  },
+  {
+    "revision": "ae06e5a3e5a50c4ae349a4d24a54f6a7",
+    "url": "/SPA-with-React-Router/static/media/coconutleaf2.ae06e5a3.jpg"
+  },
+  {
+    "revision": "ea3d697c3a26fff0b7f5b72fad8014d8",
+    "url": "/SPA-with-React-Router/static/media/item1.ea3d697c.jpg"
+  },
+  {
+    "revision": "6468ceda4da68a40e78d44598e12f045",
+    "url": "/SPA-with-React-Router/static/media/item2.6468ceda.jpg"
+  },
+  {
+    "revision": "69accebe8dddbafdd63d580df325cf3b",
+    "url": "/SPA-with-React-Router/static/media/item3.69accebe.jpg"
+  },
+  {
+    "revision": "6e87d3071c5e7ca55425ee51c2e5fb35",
+    "url": "/SPA-with-React-Router/static/media/zdj3c.6e87d307.jpg"
+  },
+  {
+    "revision": "1f19aa4a097ce4c9f1e0325aaebd0115",
+    "url": "/SPA-with-React-Router/static/media/zdj4c.1f19aa4a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SPA-with-React-Router/./precache-manifest.25a8d2aa426a3d4835e24eabbde965cd.js"
+  "/SPA-with-React-Router/precache-manifest.1e5e12fcc86a91f51e91bb2fe9e4e048.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SPA-with-React-Router/./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SPA-with-React-Router/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
