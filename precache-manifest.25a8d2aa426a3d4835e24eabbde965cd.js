eb7c849619d1cd413c5d7525a7701e5e97ba5b96
Ola Pawelec <olapawelec@MacBook-Pro-MacOS.local>
2020-03-13 17:22:23 +0100

    Updates

---
+++ precache-manifest.25a8d2aa426a3d4835e24eabbde965cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805551d011ed75953c7df5102b71613d",
+    "url": "/SPA-with-React-Router/./index.html"
+  },
+  {
+    "revision": "9f521227a2d687b08258",
+    "url": "/SPA-with-React-Router/./static/css/main.7bdc269b.chunk.css"
+  },
+  {
+    "revision": "3cca412c8907a136624a",
+    "url": "/SPA-with-React-Router/./static/js/2.57eaccdd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/SPA-with-React-Router/./static/js/2.57eaccdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f521227a2d687b08258",
+    "url": "/SPA-with-React-Router/./static/js/main.bea40377.chunk.js"
+  },
+  {
+    "revision": "65ec43c61bca7d0d127c",
+    "url": "/SPA-with-React-Router/./static/js/runtime-main.41ba45e4.js"
+  },
+  {
+    "revision": "ae06e5a3e5a50c4ae349a4d24a54f6a7",
+    "url": "/SPA-with-React-Router/./static/media/coconutleaf2.ae06e5a3.jpg"
+  },
+  {
+    "revision": "ea3d697c3a26fff0b7f5b72fad8014d8",
+    "url": "/SPA-with-React-Router/./static/media/item1.ea3d697c.jpg"
+  },
+  {
+    "revision": "6468ceda4da68a40e78d44598e12f045",
+    "url": "/SPA-with-React-Router/./static/media/item2.6468ceda.jpg"
+  },
+  {
+    "revision": "69accebe8dddbafdd63d580df325cf3b",
+    "url": "/SPA-with-React-Router/./static/media/item3.69accebe.jpg"
+  },
+  {
+    "revision": "6e87d3071c5e7ca55425ee51c2e5fb35",
+    "url": "/SPA-with-React-Router/./static/media/zdj3c.6e87d307.jpg"
+  },
+  {
+    "revision": "1f19aa4a097ce4c9f1e0325aaebd0115",
+    "url": "/SPA-with-React-Router/./static/media/zdj4c.1f19aa4a.jpg"
+  }
+]);
